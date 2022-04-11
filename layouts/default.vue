1800acf7c130418d33466b42ba853ec6c25a8532
YUAN Shen <yuanshenmick@gmail.com>
2022-04-11 19:39:10 +0800

    feat: [wip]timestamp

--- layouts/default.vue
+++ layouts/default.vue
@@ -81,6 +81,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Timestamp',
+          to: '/converter/timestamp',
+        },
       ],
       miniVariant: false,
       right: true,
---
+++ pages/converter/timeStamp.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row>
+    <v-col class="text-center"> time </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'TimeStamp',
+}
+</script>
