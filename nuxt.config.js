5315fbc5f6424b92599f6d517c8020f074facead
YUAN Shen <yuanshenmick@gmail.com>
2022-04-12 01:23:08 +0800

    chore: baidu verification

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'baidu-site-verification', content: 'code-7pnylDNzlc' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
