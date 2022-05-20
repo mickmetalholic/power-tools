c3c71d20a0b8a886bd89ab1cd0c5f3dd5f3208c6
YUAN Shen <yuanshenmick@gmail.com>
2022-05-21 02:19:27 +0800

    xxx

--- common/constants/utilList.ts
+++ common/constants/utilList.ts
@@ -1,49 +1,71 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
+enum CATEGORY {
+  CONVERTER = 'converter'
+}
+
 export const UTIL_LIST = [
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   },
   {
     title: 'Timestamp Converter',
     path: '/converter/timestamp',
     Icon: AccessTimeIcon,
+    desc: 'timestamp coverter timestamp converter',
+    category: CATEGORY.CONVERTER,
   }
 ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
 
   return (
     <Grid container spacing={2}>
-      {UTIL_LIST.map(({ path, title }, index) => (
+      {UTIL_LIST.map(({ path, title, desc, category, Icon }, index) => (
         <Grid item xs={3} key={index}>
           <Card
             key={index}
@@ -22,8 +22,68 @@ const Home: NextPage = () => {
             <Link href={path}>
               <CardActionArea>
                 <CardContent>
-                  <Typography sx={{ fontSize: 18 }} color="text.primary" gutterBottom variant="h5">
-                    {title}
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {category}
+                  </Typography>
+                  <Grid
+                    container
+                    spacing={2}
+                    style={{
+                      marginTop: '0px',
+                    }}
+                  >
+                    <Grid
+                      item
+                      xs={2}
+                      style={{
+                        height: '40px',
+                        paddingTop: 0,
+                        position: 'relative',
+                      }}
+                    >
+                      <Icon
+                        fontSize="large"
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          bottom: 0,
+                          margin: 'auto',
+                        }}
+                      />
+                    </Grid>
+                    <Grid
+                      item
+                      xs={10}
+                      style={{
+                        height: '40px',
+                        paddingTop: 0,
+                        paddingLeft: '10px',
+                      }}
+                    >
+                      <Typography
+                        color="text.primary"
+                        gutterBottom
+                        variant="h5"
+                        style={{
+                          margin: 0,
+                          paddingTop: 0,
+                          lineHeight: '40px',
+                        }}
+                      >
+                        {title}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                  <Typography
+                    color="text.primary"
+                    gutterBottom
+                    variant="body2"
+                    sx={{
+                      height: '60px',
+                      marginTop: '20px'
+                    }}
+                  >
+                    {desc}
                   </Typography>
                 </CardContent>
               </CardActionArea>
